82808f8e72558373da5bdbf74f6a6b18dec01eaa
Francois Hill <francoishill11@gmail.com>
2019-08-22 06:23:34 +0200

    fix: comment out ConsoleApp/Program code for now

--- TimeFlyTrap.ConsoleApp/Program.cs
+++ TimeFlyTrap.ConsoleApp/Program.cs
@@ -14,53 +14,54 @@ namespace TimeFlyTrap.ConsoleApp
         // ReSharper disable once UnusedParameter.Local
         static void Main(string[] args)
         {
-            _tracker = new ActiveWindowsTracker();
-
-            _tracker.StartTicker(new TrackingListener());
-
-            Console.CancelKeyPress += ConsoleOnCancelKeyPress;
-
-            while (true)
-            {
-                Thread.Sleep(TimeSpan.FromMilliseconds(500));
-            }
-
-            // ReSharper disable once FunctionNeverReturns
+            throw new NotImplementedException();
+//            _tracker = new ActiveWindowsTracker();
+//
+//            _tracker.StartTicker(new TrackingListener());
+//
+//            Console.CancelKeyPress += ConsoleOnCancelKeyPress;
+//
+//            while (true)
+//            {
+//                Thread.Sleep(TimeSpan.FromMilliseconds(500));
+//            }
+//
+//            // ReSharper disable once FunctionNeverReturns
         }
 
-        private class TrackingListener : ITrackingListener
-        {
-            public void OnActiveWindowInfo(OnActiveWindowInfoEvent @event)
-            {
-                Console.WriteLine($"New title: {@event.Title}, Module: {@event.ModuleFilePath}, Startup: {@event.SystemStartupTime}, IdleDuration: {@event.IdleDuration}");
-            }
-        }
-
-        private static void ConsoleOnCancelKeyPress(object sender, ConsoleCancelEventArgs e)
-        {
-            if (_tracker.StopAndGetReport(out var report))
-            {
-                //TODO: Should get from IAppFilePathProvider.LocalAppData
-                var dir = Path.Combine(
-                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-                    "TimeFlyTrap",
-                    $"{DateTime.Now:yyyy-MM-dd}");
-
-                if (!Directory.Exists(dir))
-                {
-                    Directory.CreateDirectory(dir);
-                }
-
-                var filePathWithoutExtension = Path.Combine(dir, $"{DateTime.Now:yyyy-MM-dd HH_mm_ss}");
-
-                ActiveWindowsTracker.SaveReportsToJsonAndHtml(
-                    new ObservableCollection<WindowTimes>(report.Values),
-                    filePathWithoutExtension + ".json",
-                    filePathWithoutExtension + ".html");
-            }
-
-            Console.WriteLine("Monitoring aborted, press any key to close");
-            Console.ReadKey();
-        }
+//        private class TrackingListener : ITrackingListener
+//        {
+//            public void OnActiveWindowInfo(OnActiveWindowInfoEvent @event)
+//            {
+//                Console.WriteLine($"New title: {@event.Title}, Module: {@event.ModuleFilePath}, Startup: {@event.SystemStartupTime}, IdleDuration: {@event.IdleDuration}");
+//            }
+//        }
+//
+//        private static void ConsoleOnCancelKeyPress(object sender, ConsoleCancelEventArgs e)
+//        {
+//            if (_tracker.StopAndGetReport(out var report))
+//            {
+//                //TODO: Should get from IAppFilePathProvider.LocalAppData
+//                var dir = Path.Combine(
+//                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+//                    "TimeFlyTrap",
+//                    $"{DateTime.Now:yyyy-MM-dd}");
+//
+//                if (!Directory.Exists(dir))
+//                {
+//                    Directory.CreateDirectory(dir);
+//                }
+//
+//                var filePathWithoutExtension = Path.Combine(dir, $"{DateTime.Now:yyyy-MM-dd HH_mm_ss}");
+//
+//                ActiveWindowsTracker.SaveReportsToJsonAndHtml(
+//                    new ObservableCollection<WindowTimes>(report.Values),
+//                    filePathWithoutExtension + ".json",
+//                    filePathWithoutExtension + ".html");
+//            }
+//
+//            Console.WriteLine("Monitoring aborted, press any key to close");
+//            Console.ReadKey();
+//        }
     }
 }
